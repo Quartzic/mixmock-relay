7c9d3f1bdfb37ac7163e92252a30ea5b8ab2e70f
Tolu Akinbo <70776613+taokinbo@users.noreply.github.com>
2023-09-17 10:27:54 -0400

    Update relay.py
    made ingredient parsing and interaction

--- relay.py
+++ relay.py
@@ -42,7 +42,8 @@ scrollbar.config(command=log.yview)
 
 def writeLog(text):
     # add timestamp prefix
-    text = datetime.now().strftime("%m/%d/%Y %-I:%M:%S%p") + " " + text
+    # text = datetime.now().strftime("%m/%d/%Y %-I:%M:%S%p") + " " + text
+    # text = "debug" + text
     log.insert("end", text + "\n")
     print(text)
     log.see("end")
@@ -60,9 +61,11 @@ ser.baudrate = 28800
 ser.timeout = 0.5
 
 ports = list(serial.tools.list_ports.comports())
+print(ports)
 likely_ports = []
 for i, p in enumerate(ports):
-    if "Giga" in p.description:
+    print(p.description)
+    if "Giga" in p.description or "Serial" in p.description:
         likely_ports += [p]
 
 if len(likely_ports) == 0:
@@ -77,6 +80,15 @@ if len(likely_ports) > 0:
     ser.port = likely_ports[0].device
     ser.open()
 
+fullIds = {
+        "LEMON_SELTZER": 1,
+        "KIWI_STRAWBERRY_SNAPPLE": 2,
+        "COCA_COLA": 3,
+        "ORANGE_JUICE": 4,
+        "MOCHA_ICED_COFFEE": 5,
+        "BLUE_POWERADE": 6
+}
+
 
 def writeSerial():
     if ser.is_open:
@@ -84,23 +96,32 @@ def writeSerial():
             # hit the endpoint with requests
             r = requests.get(config["endpoint"])
             if r.status_code == 200:
-                writeLog(f"Successfully hit endpoint, got drink {r.json()['drinkInformation']['name']}")
+                ingSize = len(r.json()['drinkInformation']['ingredients'])
+                for key, value in r.json()['drinkInformation']['ingredients']:
+                    writeLog(fullIds[value['id']])
+                    writeLog(value['amount'])
             else:
-                writeLog("Error hitting endpoint")
+                print("Error hitting endpoint")
+                writeLog("1")
+                writeLog("1")
 
-            ser.write(r.json()["drinkInformation"]["name"].encode("utf-8"))
+            # ser.write(r.json()["drinkInformation"]["name"].encode("utf-8"))
+            # ser.flush()
+
+            # ser.write("9".encode("utf-8"))
 
         except KeyboardInterrupt:
             ser.close()
             exit()
     else:
         writeLog("Serial port was closed. You must restart the Uplink Relay.")
-    
-    root.after(1500, writeSerial)
+
+    root.after(5000, writeSerial)
 
 if ser.is_open:
     writeLog("Connected to " + ser.port)
     connectionLabel.config(text=f"Connected to {ser.port}")
+    ser.flush()
     root.after(1500, writeSerial)
 
 root.mainloop()
